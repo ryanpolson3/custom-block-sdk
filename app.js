de3c39524938ebc318f5859464d3dfc5735076bb
Ryan <ryanpolson3@gmail.com>
2021-11-11 14:30:37 -0600

    added procfile and changed port#

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log('Hello from node.js...');
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
