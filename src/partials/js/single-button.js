0e10800d451e5dc2e30a6b140c791e9eeeb7174b
Ryan <ryanpolson3@gmail.com>
2021-12-14 16:56:01 -0600

    Updated single-button.js - removed any word of single from all variables.

--- src/partials/js/single-button.js
+++ src/partials/js/single-button.js
@@ -2,15 +2,15 @@ export default function template(data){
     return `
 <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
 	<tr>
-		<td style="text-align: center; padding-top:${data.marginTopSingle}px !important; padding-bottom: ${data.marginBottomSingle}px !important; padding-left:${data.centerButtonSingle ? '0' : '30'}px !important;">
-			<table border="0" cellpadding="0" cellspacing="0" role="presentation" style="${data.centerButtonSingle ? '' : 'float:left;'}">
+		<td style="text-align: center; padding-top:${data.marginTop}px !important; padding-bottom: ${data.marginBottom}px !important; padding-left:${data.centerButton ? '0' : '30'}px !important;">
+			<table border="0" cellpadding="0" cellspacing="0" role="presentation" style="${data.centerButton ? '' : 'float:left;'}">
 				<tbody>
 					<tr>
 						<td class="button-td button-td-primary" style="background: #${data.backgroundColorSingle};border-radius: 4px;">
 							<a class="button-a button-a-primary"
 								alias="${data.buttonUrl}"
 								href="${data.buttonUrl}"
-								style="font-family: Arial, sans-serif; font-size: ${data.fontSizeSingle}px; line-height: ${data.lineHeightSingle}px; font-weight: ${data.fontWeightSingle}; text-decoration: none; padding: 5px 25px; display: block; color: #${data.fontColorSingle}; border: 1px solid #${data.borderColorSingle}; background: #${data.backgroundColorSingle}; border-radius: 4px;"
+								style="font-family: Arial, sans-serif; font-size: ${data.fontSize}px; line-height: ${data.lineHeight}px; font-weight: ${data.fontWeight}; text-decoration: none; padding: 5px 25px; display: block; color: #${data.fontColor}; border: 1px solid #${data.borderColor}; background: #${data.backgroundColor}; border-radius: 4px;"
 								target="_blank">${data.buttonText}
 							</a>
 						</td>
