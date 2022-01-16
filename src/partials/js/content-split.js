18d8111e595213b697301cce93e4324837aea34d
Ryan <ryanpolson3@gmail.com>
2022-01-16 13:45:31 -0600

    Update: Content-Split

--- src/partials/js/content-split.js
+++ src/partials/js/content-split.js
@@ -2,12 +2,15 @@ import BUTTON from "./button.js";
 
 export default function template(data) {
   return `
+  <!--[if (gte mso 9)|(IE)]><table role="presentation" align="center" border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td align="center" valign="top" width="100%"><![endif]-->
   <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
         <tr>
             <td dir="${data.imageAlignmentRight ? "rtl" : "ltr"}" style="padding: 0; background-color: ${data.backgroundColor};" width="100%">
+            <!--[if (gte mso 9)|(IE)]><table role="presentation" align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="width: 100%;"><tr><td align="center" valign="top" width="100%"><![endif]-->
                 <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
                     <tbody>
                         <tr>
+                            <!--[if (gte mso 9)|(IE)]><table align="center" border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td align="left" valign="top" width="50%"><![endif]-->
                             <!-- Column : BEGIN -->
                             <th class="stack-column-center" width="50%">
                                 <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
@@ -25,6 +28,8 @@ export default function template(data) {
                                 </table>
                             </th>
                             <!-- Column : END -->
+
+                            <!--[if (gte mso 9)|(IE)]></td><td align="left" valign="top" width="50%"><![endif]-->
                             <!-- Column : BEGIN -->
                             <th class="stack-column-center" width="50%">
                                 <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
@@ -50,11 +55,14 @@ export default function template(data) {
                                 </table>
                             </th>
                             <!-- Column : END -->
+                            <!--[if (gte mso 9)|(IE)]></td></tr></table><![endif]-->
                         </tr>
                     </tbody>
                 </table>
+                <!--[if (gte mso 9)|(IE)]></td></tr></table><![endif]-->
             </td>
         </tr>
   </table>
+  <!--[if (gte mso 9)|(IE)]></td></tr></table><![endif]-->
   `;
 }
